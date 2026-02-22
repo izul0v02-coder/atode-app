12a4d8f2598b1d1f754e007d71c64889c0bfc1d0
izul0v02-coder <izu.l0v02@gmail.com>
2026-02-22 23:42:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'atode-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'atode-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
